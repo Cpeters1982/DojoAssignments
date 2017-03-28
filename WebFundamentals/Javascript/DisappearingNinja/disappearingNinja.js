387327681c524ef18e0e33fb4ec578dbfb09a00f
cosmicbuffalo <cosmic.buffalo.dev@gmail.com>
2017-03-28 15:01:33 -0500

    added Disappearing Ninja assignment

---
+++ WebFundamentals/Javascript/DisappearingNinja/disappearingNinja.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $("img").click(function(){
+        $(this).css("visibility", "hidden");
+    })
+    $("button").click(function(){
+        $("img").css("visibility","visible");
+    })
+
+
+})
